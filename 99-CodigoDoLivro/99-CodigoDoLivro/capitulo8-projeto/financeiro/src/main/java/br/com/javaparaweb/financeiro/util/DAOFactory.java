fd1349e510cd9efcaccde8f201f1dd5e5cf55a23
José Stélio R. Malcher Junior <contato@josemalcher.net>
2017-10-27 17:29:54 -0300

    projeto base com códigos do livro

---
+++ 99-CodigoDoLivro/99-CodigoDoLivro/capitulo8-projeto/financeiro/src/main/java/br/com/javaparaweb/financeiro/util/DAOFactory.java
@@ -0,0 +1,18 @@
+package br.com.javaparaweb.financeiro.util;
+import br.com.javaparaweb.financeiro.usuario.*;
+import br.com.javaparaweb.financeiro.conta.*;
+public class DAOFactory {
+
+	public static UsuarioDAO criarUsuarioDAO() {
+		UsuarioDAOHibernate usuarioDAO = new UsuarioDAOHibernate();
+		usuarioDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+		return usuarioDAO; 
+	}
+	
+	public static ContaDAO criarContaDAO() { 
+		ContaDAOHibernate contaDAO = new ContaDAOHibernate();
+		contaDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+		return contaDAO;
+	}
+
+}
